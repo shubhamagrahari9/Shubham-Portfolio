3b8338cc8394cccaee2bb79827c1a82d0a947a3a
Shubham Agrahari <Shubham.hari78@gmail.com>
2024-07-11 12:10:11 +0530

    New File Added

---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "Shubham";
+const meta = {
+    title: "Shubham Agarahari",
+    description: "I’m Shubham Agrahari Full stack devloper.",
+};
+
+const introdata = {
+    title: "I’m Shubham Agrahari",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am looking for Job and internship opportunities.",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+   your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: " I have completed my Masters in Masters of Computer Application from Lal Bahadur Shastri Institute, Lucknow. Currently, I am looking for Job opportunities.",
+};
+const worktimeline = [{
+        title: "MERN Full Stack Development",
+        where: "Apna College",
+        date: "2023",
+    },
+    {
+        title: "Market Research",
+        where: "Mirats Quanto",
+        date: "2021",
+    },
+    {
+        title: "Data Analyst",
+        where: "GeeksForGeeks",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 50,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "Jquery",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "To Do List",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "ShopCart",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Calculator",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Tic Tac Toe Game",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+   
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "agnihotri.shubham2017@gmail.com",
+    YOUR_FONE: "+91 8840109721",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
